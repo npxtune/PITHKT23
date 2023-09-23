ce4bec78c534b7647c1b5d7580b798d7b14ac967
Tim <horochowiktim@pm.me>
2023-09-23 13:27:23 +0200

    fix load scene

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -1,35 +1,28 @@
+using System.Transactions;
 using Godot;
-using System;
-using System.Linq;
+namespace PITHKT23.scripts;
 
 public partial class GameManager : Node
-{
-	// Called when the node enters the scene tree for the first time.
+{ 
 	private static int _nextScene;
-	
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public void _Process(double delta)
-	{
-		GD.Print("fuuuuck");
-	}
-
-	public void _ReloadScene()
+	private void ReloadScene()
 	{
 		var reloadScene = Globals.listCounter;
 		var sceneToLoad = Globals.scenes[reloadScene];
-		PackedScene scene = GD.Load<PackedScene>("res://scenes/" + sceneToLoad + ".tscn");
+		var scene = ResourceLoader.Load<PackedScene>("res://scenes/" + sceneToLoad + ".tscn");
 		GetTree().ChangeSceneToFile("res://scenes/" + sceneToLoad + ".tscn");
 	}
 
-	public void _LoadNextScene(int indexofScenetoLoad = -1)
+	public void LoadNextScene(int loadSceneIndex = -1)
 	{
-		if (indexofScenetoLoad == -1)
+		var currentScene = GetTree().CurrentScene;
+		if (loadSceneIndex == -1)
 		{
 			_nextScene =  Globals.listCounter + 1; 
 		}
 		else
 		{
-			_nextScene = indexofScenetoLoad; 
+			_nextScene = loadSceneIndex; 
 		}
 		if (Globals.scenes.Count == Globals.listCounter + 1)
 		{
@@ -39,24 +32,13 @@ public partial class GameManager : Node
 		Globals.listCounter = _nextScene;
 		var sceneToLoad = Globals.scenes[_nextScene];
 		GD.Print(sceneToLoad);
-		PackedScene scene = GD.Load<PackedScene>("res://scenes/" + sceneToLoad + ".tscn");
-		GetTree().ChangeSceneToPacked(scene);
-		
-		//PackedScene scene = GD.Load<PackedScene>("res://scenes/" + sceneToLoad + ".tscn");
-		//GetTree().ChangeSceneToFile("res://scenes/" + sceneToLoad + ".tscn");
-		//var next_level_resource = LoadScene("res://scenes/" + sceneToLoad + ".tscn");
-		//GetTree().ChangeSceneToFile("res://scenes/" + sceneToLoad + ".tscn");
-
-		// Changing does not work
-
-
-
+		var scene = ResourceLoader.Load<PackedScene>("res://scenes/" + sceneToLoad + ".tscn").Instantiate();
+		GetTree().Root.AddChild(scene);
+		GetTree().Root.RemoveChild(currentScene);
 	}
 
-	public void _allLevelCompleted()
+	private static void _allLevelCompleted()
 	{
 		GD.Print("All level completed");
 	}
 }
-
-
