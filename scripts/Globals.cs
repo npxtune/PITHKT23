488cac6d9e24aa64e375edec19d75865887d7a12
Tim <horochowiktim@pm.me>
2023-09-23 14:25:34 +0200

    Revert "fix repo"
    This reverts commit bf64ae6df44c19431d9997deefad0f4fa75a0260.

--- scripts/Globals.cs
+++ scripts/Globals.cs
@@ -1,8 +1,12 @@
+using Godot;
+using System;
 using System.Collections.Generic;
-namespace PITHKT23.scripts;
 
 public static class Globals
 {
-	public static readonly List<string> Scenes = new List<string>(){"GameManagerTutorial", "GameManagerLevel1", "GameManagerLevel2"};
-	public static int ListCounter = -1;
+	public static List<string> scenes = new List<string>(){"GameManagerTutorial", "GameManagerLevel1", "GameManagerLevel2"};
+	public static int listCounter = -1;
+	
+	
+	
 }
