cee6df0616a8fa5d6955ca22a475c9440edeb6bd
THMKruemel <lennartsigge@icloud.com>
2023-09-23 21:38:49 +0200

    add texture to main menu add next: main menu arcade image

--- scripts/Globals.cs
+++ scripts/Globals.cs
@@ -3,6 +3,6 @@ namespace PITHKT23.scripts;
 
 public static class Globals
 {
-	public static readonly List<string> Scenes = new List<string>(){"/GameScenes/Scene2", "/GameScenes/Scene3", "/GameScenes/Scene4"};
+	public static readonly List<string> Scenes = new List<string>(){"/GameScenes/Scene2", "/GameScenes/Scene3", "/GameScenes/Scene4", "/GameScenes/GameFinished"};
 	public static int ListCounter = -1;
 }
