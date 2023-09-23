25db65cc59b838c6f4800325fc3776216c93aa98
Tim <horochowiktim@pm.me>
2023-09-23 13:34:19 +0200

    fixed cedrics bullshit

--- scripts/CollisionEvents.cs
+++ scripts/CollisionEvents.cs
@@ -3,8 +3,12 @@ namespace PITHKT23.scripts;
 
 public partial class CollisionEvents : Area2D
 {
-	private void OnSpikesBodyEntered(PhysicsBody2D body)
+	private SceneModifiers _sceneModifiers;
+	private void OnSpikesBodyEntered(CollisionObject2D body)
 	{
-		GD.Print("collided with area");
+		_sceneModifiers = GetNode<SceneModifiers>("/root/world/scene_modifiers");
+		if (body is not CharacterBody2D || _sceneModifiers.CollisionModifier == true) return;
+		GD.Print("Game over.");
+		GetTree().ChangeSceneToFile(GetTree().CurrentScene.SceneFilePath);
 	}
 }
--- scripts/SceneModifiers.cs
+++ scripts/SceneModifiers.cs
@@ -4,8 +4,7 @@ namespace PITHKT23.scripts;
 public partial class SceneModifiers : Node
 {
 	[ExportGroup("Modifiers")]
-	[Export]
-	public float GravityModifier = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-	[Export]
-	public float JumpModifier = -400.0f;
+	[Export] public float GravityModifier = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	[Export] public float JumpModifier = -400.0f;
+	[Export] public bool CollisionModifier = false;
 }
