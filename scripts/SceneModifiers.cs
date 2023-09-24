87fa8c8de5eb714b0c89737a9f90d9f27644aff7
Tim <horochowiktim@pm.me>
2023-09-24 10:00:44 +0200

    polish scenemodifiers.cs

--- scripts/SceneModifiers.cs
+++ scripts/SceneModifiers.cs
@@ -3,6 +3,10 @@ namespace PITHKT23.scripts;
 
 public partial class SceneModifiers : Node
 {
+    //  STANDARD VALUES DO NOT CHANGE!!!
+    private const float Gravity = 980f;
+    private const float Jump = -400f;
+    
     [ExportGroup("Modifiers")]
     [Export] public float GravityModifier = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
     [Export] public float JumpModifier = -400.0f;
@@ -12,20 +16,24 @@ public partial class SceneModifiers : Node
     private void GravitySliderChanged(float value)
     {
         GD.Print(GravityModifier * (value / 50));
+        GravityModifier = Gravity * (value / 50);
     }
 
     private void JumpSliderChanged(float value)
     {
         GD.Print(JumpModifier * (value / 50));
+        JumpModifier = Jump * (value / 50);
     }
 
     private void CollisionBoolChanged(bool isPressed)
     {
-        // TODO button press
+        GD.Print(isPressed);
+        CollisionModifier = isPressed;
     }
 
     private void MoveLevelBoolChanged(bool isPressed)
     {
-        // TODO button press + move level??
+        GD.Print(isPressed);
+        MoveLevelModifier = isPressed;
     }
 }
