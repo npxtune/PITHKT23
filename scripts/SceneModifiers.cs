fd7036c0d74ae348763d405f920ba1767bd59213
Tim <horochowiktim@pm.me>
2023-09-24 09:51:36 +0200

    polish scenemodifiers.cs

--- scripts/SceneModifiers.cs
+++ scripts/SceneModifiers.cs
@@ -8,21 +8,6 @@ public partial class SceneModifiers : Node
     [Export] public float JumpModifier = -400.0f;
     [Export] public bool CollisionModifier = false;
     [Export] public bool MoveLevelModifier = false;
-    
-    // public override void _Process(double delta)
-    // {
-    //     //Getting data from Sliders and Buttons
-    //     Godot.Range G_Slider_Range = GetNode<Godot.Range>("Gravity-Slider");
-    //     Godot.Range J_Slider_Range = GetNode<Godot.Range>("Jump-Slider");
-    //     BaseButton InvertSpikes_Button = GetNode<BaseButton>("InvertSpikes");
-    //     BaseButton Move_Level_Button = GetNode<BaseButton>("Move_Level");
-    //     BaseButton Invert_Flip_Button = GetNode<BaseButton>("Invert_Flip");
-    //     GravityModifier = baseGravity  * (float)((G_Slider_Range.Value) / 50) * flipGravity;
-    //     JumpModifier = baseJump  * (float)((J_Slider_Range.Value) / 50);
-    //     Move_Level = InvertSpikes_Button.ButtonPressed;
-    //     Invert_Flip = InvertSpikes_Button.ButtonPressed;
-    //     CollisionModifier = InvertSpikes_Button.ButtonPressed;
-    // }
 
     private void GravitySliderChanged(float value)
     {
