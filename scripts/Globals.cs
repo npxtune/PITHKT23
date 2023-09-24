f750708914064fd883f0c310d4678baca8fba70d
Tim <horochowiktim@pm.me>
2023-09-24 10:54:27 +0200

    scene oder

--- scripts/Globals.cs
+++ scripts/Globals.cs
@@ -3,6 +3,6 @@ namespace PITHKT23.scripts;
 
 public static class Globals
 {
-	public static readonly List<string> Scenes = new List<string>(){"/GameScenes/Scene1","/GameScenes/Scene2", "/GameScenes/Scene3", "/GameScenes/Scene4", "/GameScenes/Scene5", "/GameScenes/Scene6", "/GameScenes/GameFinished"};
+	public static readonly List<string> Scenes = new List<string>(){"/GameScenes/Scene1","/GameScenes/Scene2", "/GameScenes/Scene3", "/GameScenes/Scene5", "/GameScenes/Scene6", "/GameScenes/GameFinished"};
 	public static int ListCounter = -1;
 }
